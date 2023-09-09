e46218c7975e34cce455be3bb412ac5bc650a173
Eric Glover <eric24g@gmail.com>
2023-09-09 02:38:38 -0400

    adding new code, task app and music app

---
+++ Task_App/main.py
@@ -0,0 +1,11 @@
+
+
+class Main:
+    
+    def __init__(self) -> None:
+        pass
+    
+    
+if __name__ == "__main__":
+    start_task = Main()
+    
---
+++ Task_App/task.py
@@ -0,0 +1,18 @@
+class Task:
+    def __init__(self) -> None:
+        pass
+    
+    def task_name(self):
+        pass
+    
+    def task_description(self):
+        pass
+    
+    def task_dueDate(self):
+        pass
+    
+    def task_priority(self):
+        pass
+    
+    def task_delete(self):
+        pass    
